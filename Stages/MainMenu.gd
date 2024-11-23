942742ce3d2a4cbb6b63996bfd2a74a3920b9f62
Phillip Phoelich <pphoelich@gmail.com>
2024-11-23 16:40:34 +0100

    Set up basic main menu scene. Missing level select.

---
+++ Stages/MainMenu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@export var _play_button: Button
+@export var _quit_button: Button
+
+@export var _game_stage: PackedScene
+
+func _ready() -> void:
+	_connect_signals()
+
+func _connect_signals() -> void:
+	_play_button.pressed.connect(_on_play)
+	_quit_button.pressed.connect(_on_quit)
+
+func _on_play() -> void:
+	get_tree().change_scene_to_packed(_game_stage)
+
+func _on_quit() -> void:
+	get_tree().quit()
